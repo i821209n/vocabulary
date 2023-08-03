d675b8d31cb3a14ec8a446a74a64dd0d2f4d3254
C.Y.Lin <83861620+i821209n@users.noreply.github.com>
2023-08-03 15:22:44 +0800

    1. complete "save_word" api. 2. complete "add_word_page". 3. connect main to gui_init.

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 #import
 import tkinter as tk
 from tkinter import font
+from tkinter import messagebox
 
 #variables
 root = None
@@ -26,14 +27,29 @@ def switch_to_first_page(cur_page):
     cur_page.pack_forget()
     first_page_frame.pack()
 
-def save_word():
-    print("save the word!!")
+def save_word(word_entry, definition_entry, sentence_entry):
+    word = word_entry.get()
+    definition = definition_entry.get()
+    sentence = sentence_entry.get()
+    if(word == ""):
+        messagebox.showwarning("Warning", "Please enter a word.")
+    elif(definition == ""):
+        messagebox.showwarning("Warning", "Please enter the definition.")
+    elif(sentence == ""):
+        messagebox.showwarning("Warning", "Please enter a sentence.")
+    else:
+        # TODO : implement save words to google sheets
+        print("save the word!!")
+        messagebox.showinfo("Saving", f"save {word} success")
+        word_entry.delete(0, tk.END)
+        definition_entry.delete(0, tk.END)
+        sentence_entry.delete(0, tk.END)
 
 def first_page_init():
     global first_page_frame
 
     first_page_frame = tk.Frame(root)
-    first_page_frame.pack()
+    # first_page_frame.pack()
 
     adding_word_btn = tk.Button(first_page_frame, text="add words", command=switch_to_adding_word_page, font=custom_font)
     adding_word_btn.pack(pady=10)
@@ -48,18 +64,27 @@ def add_word_page_init():
     global add_word_page_frame
 
     add_word_page_frame = tk.Frame(root)
-    add_word_page_frame.pack()
+    # add_word_page_frame.pack()
 
     word_label = tk.Label(add_word_page_frame, text="Enter a word:", font=custom_font)
     word_label.pack(pady=10)
 
+    word_entry = tk.Entry(add_word_page_frame, font=custom_font, width=60)
+    word_entry.pack(pady=5)
+
     definition_label = tk.Label(add_word_page_frame, text="Enter the definition:", font=custom_font)
     definition_label.pack(pady=10)
+
+    definition_entry = tk.Entry(add_word_page_frame, font=custom_font, width=60)
+    definition_entry.pack(pady=5)
 
     sentence_label = tk.Label(add_word_page_frame, text="Enter a example:", font=custom_font)
     sentence_label.pack(pady=10)
 
-    save_btn = tk.Button(add_word_page_frame, text="save", command=save_word, font=custom_font)
+    sentence_entry = tk.Entry(add_word_page_frame, font=custom_font, width=60)
+    sentence_entry.pack(pady=5)
+
+    save_btn = tk.Button(add_word_page_frame, text="save", command=lambda: save_word(word_entry, definition_entry, sentence_entry), font=custom_font)
     save_btn.pack(pady=10)
 
     back_btn = tk.Button(add_word_page_frame, text="back", command=lambda: switch_to_first_page(add_word_page_frame), font=custom_font)
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+import gui
+
 def main():
-    # Replace 'path/to/your/credentials.json' with the actual path to your credentials JSON file
-    credentials_file_path = 'path/to/your/credentials.json'
+    gui.gui_init()
+    # # Replace 'path/to/your/credentials.json' with the actual path to your credentials JSON file
+    # credentials_file_path = 'path/to/your/credentials.json'
 
-    # Replace 'YOUR_SPREADSHEET_ID' with the ID of the Google Sheets document where you want to create the worksheet
-    spreadsheet_id = 'YOUR_SPREADSHEET_ID'
+    # # Replace 'YOUR_SPREADSHEET_ID' with the ID of the Google Sheets document where you want to create the worksheet
+    # spreadsheet_id = 'YOUR_SPREADSHEET_ID'
 
-    # Replace 'New Worksheet' with the name you want to give to the new worksheet
-    worksheet_name = 'New Worksheet'
+    # # Replace 'New Worksheet' with the name you want to give to the new worksheet
+    # worksheet_name = 'New Worksheet'
 
-    # You can adjust the rows and columns as per your requirement
-    rows = 10000
-    cols = 1000
+    # # You can adjust the rows and columns as per your requirement
+    # rows = 10000
+    # cols = 1000
 
-    create_new_worksheet(credentials_file_path, spreadsheet_id, worksheet_name, rows=rows, cols=cols)
+    # create_new_worksheet(credentials_file_path, spreadsheet_id, worksheet_name, rows=rows, cols=cols)
 
 if __name__ == "__main__":
     main()
