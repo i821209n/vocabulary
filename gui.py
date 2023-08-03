0123dbf6dc1f9eb4ef63101cbf8ab7497314cc8c
C.Y.Lin <83861620+i821209n@users.noreply.github.com>
2023-08-03 16:02:04 +0800

    add "test_page_init"

--- gui.py
+++ gui.py
@@ -11,6 +11,7 @@ add_word_page_frame = None
 test_page_frame = None
 calendar_page_frame = None
 
+#apis
 def switch_to_adding_word_page():
     first_page_frame.pack_forget()
     add_word_page_frame.pack()
@@ -20,12 +21,23 @@ def switch_to_testing_page():
     test_page_frame.pack()
 
 def switch_to_calendar_page():
-    first_page_frame.pack_forget()
-    calendar_page_frame.pack()
+    # TODO : implement calendar page
+    print("calendar page")
+    messagebox.showinfo("calendar_page", "calendar page is still working!")
 
 def switch_to_first_page(cur_page):
     cur_page.pack_forget()
     first_page_frame.pack()
+
+def switch_to_choice_test_page():
+    # TODO : implement choice test
+    print("choice test")
+    messagebox.showinfo("choice_test", "choice test is still working!")
+
+def switch_to_spell_test_page():
+    # TODO : implement spell test
+    print("spell test")
+    messagebox.showinfo("spell_test", "spell test is still working!")
 
 def save_word(word_entry, definition_entry, sentence_entry):
     word = word_entry.get()
@@ -54,7 +66,7 @@ def first_page_init():
     adding_word_btn = tk.Button(first_page_frame, text="add words", command=switch_to_adding_word_page, font=custom_font)
     adding_word_btn.pack(pady=10)
 
-    testing_btn = tk.Button(first_page_frame, text="test test", command=switch_to_testing_page, font=custom_font)
+    testing_btn = tk.Button(first_page_frame, text="test", command=switch_to_testing_page, font=custom_font)
     testing_btn.pack(pady=10)
 
     calendar_btn = tk.Button(first_page_frame, text="calendar", command=switch_to_calendar_page, font=custom_font)
@@ -90,6 +102,20 @@ def add_word_page_init():
     back_btn = tk.Button(add_word_page_frame, text="back", command=lambda: switch_to_first_page(add_word_page_frame), font=custom_font)
     back_btn.pack(pady=10)
 
+def test_page_init():
+    global test_page_frame
+
+    test_page_frame = tk.Frame(root)
+
+    choice_test = tk.Button(test_page_frame, text="choice test", command=switch_to_choice_test_page, font=custom_font)
+    choice_test.pack(pady=10)
+
+    spell_test = tk.Button(test_page_frame, text="spell test", command=switch_to_spell_test_page, font=custom_font)
+    spell_test.pack(pady=10)
+
+    back_btn = tk.Button(test_page_frame, text="back", command=lambda: switch_to_first_page(test_page_frame), font=custom_font)
+    back_btn.pack(pady=10)
+
 def gui_init():
     global root, custom_font
 
@@ -106,8 +132,10 @@ def gui_init():
     first_page_init()
     # add word page 
     add_word_page_init()
+    # test page
+    test_page_init()
 
-    # Show Page 1 initially
+    # Show first page initially
     first_page_frame.pack()
 
     root.mainloop()
