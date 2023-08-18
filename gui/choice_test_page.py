00f3efefb4ae84caa2b2e299b48986b42ed2447c
ian2_lin <Ian2_Lin@pegatroncorp.com>
2023-08-18 19:35:04 +0800

    implement choice test (tmp)

--- gui/choice_test_page.py
+++ gui/choice_test_page.py
@@ -1,29 +1,65 @@
 choice_test_page_frame = None
+question_num_label = None
+next_btn = None
+answer_1 = None
+answer_2 = None
+answer_3 = None
+answer_4 = None
+question_num = 1
 
 def switch_to_choice_test_page(cur_page):
+    global question_num
     # TODO : implement choice test
     # print("choice test")
     # messagebox.showinfo("choice_test", "choice test is still working!")
+    question_num = 1
+    question_num_label.config(text=f"{question_num}/10")
+    next_btn.config(state="disabled", text="next")
     cur_page.pack_forget()
     choice_test_page_frame.pack()
 
+def disableAllAnsBnt():
+    answer_1.config(state="disabled")
+    answer_2.config(state="disabled")
+    answer_3.config(state="disabled")
+    answer_4.config(state="disabled")
+
+def enableAllAnsBnt():
+    answer_1.config(state="normal")
+    answer_2.config(state="normal")
+    answer_3.config(state="normal")
+    answer_4.config(state="normal")
+
 def next_callback():
     # TODO : implement next callback
+    global question_num
+    next_btn.config(state="disabled")
+    enableAllAnsBnt()
+    if(question_num < 10):
+        question_num += 1
+        question_num_label.config(text=f"{question_num}/10")
+    else:
+        messagebox.showinfo("done", "test done return to test page")
+        tp.switch_to_test_page(choice_test_page_frame)
     print("next button press")
-    messagebox.showinfo("next", "next callback is still working!")
-
-def answer_callback(answer_no):
+    
+def answer_callback(answer_num):
     # TODO : implement answer callback
-    print(f"answer number : {answer_no}")
-    messagebox.showinfo("answer", "answer callback is still working!")
+    next_btn.config(state="normal")
+    disableAllAnsBnt()
+    if(question_num == 10):
+        next_btn.config(text="done")
+    print(f"answer number : {answer_num}")
+    # messagebox.showinfo("answer", "answer callback is still working!")
 
 def choice_test_page_init():
-    global choice_test_page_frame
+    global choice_test_page_frame, question_num_label, next_btn, answer_1, answer_2, answer_3, answer_4
 
     choice_test_page_frame = tk.Frame(gui.root)
 
-    question_no_label = tk.Label(choice_test_page_frame, text="1/10", font=gui.custom_font)
-    question_no_label.pack(pady=10)
+    # question_num_label = tk.Label(choice_test_page_frame, text="1/10", font=gui.custom_font)
+    question_num_label = tk.Label(choice_test_page_frame, font=gui.custom_font)
+    question_num_label.pack(pady=10)
 
     question_label = tk.Label(choice_test_page_frame, text="question", font=gui.custom_font)
     question_label.pack(pady=10)
